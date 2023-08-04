a769def4b8f4bdca2e61f15cbc2817e46eed9968
Inventus-s <123208378+Inventus-s@users.noreply.github.com>
2023-08-04 16:20:16 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './Components/Card/Card'
+import Grid from './Components/Grid/Grid'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Grid numberOfCards={9}/>
+    </>
+  )
+}
+
+export default App
